3a3d2b01573aa7c5216d58a332cd00c46f91e4b1
Fernando Macedo <fsoares0055@gmail.com>
2021-06-14 19:54:15 -0300

    Inicio do Projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO.Series
+{
+    public abstract class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
