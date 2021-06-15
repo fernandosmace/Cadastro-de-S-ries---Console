9b5a687ad7f82266aa68b6a35b5b24283910cde7
Fernando Macedo <fsoares0055@gmail.com>
2021-06-14 21:27:36 -0300

    Add SeriesRepository

---
+++ Classes/SeriesRepository.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using DIO.Series.Classes;
+
+namespace DIO.Series.Interfaces
+{
+    public class SeriesRepository : IRepository<Serie>
+    {
+        private List<Serie> listSeries = new List<Serie>();
+
+        public void Delete(int id)
+        {
+            listSeries[id].Delete();
+        }
+
+        public Serie GetSerieById(int id)
+        {
+            return listSeries[id];
+        }
+
+        public void Insert(Serie entity)
+        {
+            listSeries.Add(entity);
+        }
+
+        public List<Serie> List()
+        {
+            return listSeries;
+        }
+
+        public int NextId()
+        {
+            return listSeries.Count;
+        }
+
+        public void Update(int id, Serie entity)
+        {
+            listSeries[id] = entity;
+        }
+    }
+}
